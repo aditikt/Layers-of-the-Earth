7556263f969a57512432c57734e4a4ad71e257cc
riyaparitala-jpg <riya.paritala@gmail.com>
2026-02-28 19:13:58 -0500

    made some changes

--- layers-of-the-earth/Start Game/main.gd
+++ layers-of-the-earth/Start Game/main.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
-
-
-
-func _on_button_2_pressed() -> void:
+#func _on_button_2_pressed():
+	#print("Button pressed!")
+	#get_tree().change_scene_to_file("res://node_2d.tscnres://node_2d.tscn")
+#func _on_pressed():
+	#print("Pressed!")
+#
+#func _ready():
+	#%Button2.pressed.connect(_on_pressed)
+	
+func _on_button_2_pressed():
+	print("Button pressed!")
 	get_tree().change_scene_to_file("res://node_2d.tscn")
+	
