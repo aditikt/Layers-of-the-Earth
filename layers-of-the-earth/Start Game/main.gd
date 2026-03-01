308e511c899aa492257f6ad32ce9c3dcbafd170b
riyaparitala-jpg <riya.paritala@gmail.com>
2026-02-28 19:29:04 -0500

    edited

--- layers-of-the-earth/Start Game/main.gd
+++ layers-of-the-earth/Start Game/main.gd
@@ -9,7 +9,13 @@ extends Node2D
 #func _ready():
 	#%Button2.pressed.connect(_on_pressed)
 	
-func _on_button_2_pressed():
-	print("Button pressed!")
-	get_tree().change_scene_to_file("res://node_2d.tscn")
+#func _on_button_2_pressed():
+	#print("Button pressed!")
+	#get_tree().change_scene_to_file("res://node_2d.tscn")
 	
+func _ready():
+	print("Script is loaded")
+	$Button.pressed.connect(_on_button_pressed)
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://node_2d.tscn")
